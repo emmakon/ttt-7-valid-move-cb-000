c9c9a74b1c07a865c3bdab15f9e1786e07d5ed9c
Emma Konrad <emma.konrad92@gmail.com>
2018-08-09 14:02:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] = nil
-    return false
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return FALSE
   else
     return true
   end
