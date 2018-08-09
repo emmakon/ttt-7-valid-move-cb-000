90bae3e2fd499d2addc562605b66d6ee59afba50
Emma Konrad <emma.konrad92@gmail.com>
2018-08-09 14:04:19 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,10 +7,8 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?
+  if position_taken? && index.between(1, 9) != true
     return false
-  elsif index.between(1, 9) 
-    return true
   else
     return true
   end
